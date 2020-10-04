97ae4c515a13c73de046a565eb0511d435dc76b7
eldadmamo <eldadf456@gmail.com>
2020-10-04 09:27:17 -0700

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 // feature 1
 import React from 'react';
+import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super ();
     this.state = {
       products: data.products,
       size: '',
       sort: '',
     };
   }
-  render() {
+  sortProducts = event => {
+    // impl
+    const sort = event.target.value;
+    console.log (event.target.value);
+    this.setState (state => ({
+      sort: sort,
+      products: this.state.products
+        .slice ()
+        .sort (
+          (a, b) =>
+            sort === 'lowest'
+              ? a.price > b.price ? 1 : -1
+              : sort === 'highest'
+                  ? a.price < b.price ? 1 : -1
+                  : a._id < b._id ? 1 : -1
+        ),
+    }));
+  };
+  filterProducts = event => {
+    //impl
+    console.log (event.target.value);
+    if (event.target.value === '') {
+      this.setState ({size: event.target.value, product: data.products});
+    } else {
+      this.setState ({
+        size: event.target.value,
+        products: data.products.filter (
+          product => product.availableSizes.indexOf (event.target.value) >= 0
+        ),
+      });
+    }
+  };
+  render () {
     return (
       <div className="grid-container">
         <header>
@@ -21,9 +54,16 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-              <Products products={this.state.products}></Products>
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
+              <Products products={this.state.products} />
             </div>
-            <div classNme="sidebar">
+            <div className="sidebar">
               Cart Items
             </div>
           </div>
